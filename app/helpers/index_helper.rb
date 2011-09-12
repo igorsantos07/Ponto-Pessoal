c9f222d65099c084b54a6cd0506f98e585d39f4c
Igoru <igorsantos07@gmail.com>
2011-09-12 01:25:45 -0300

    fixing problem where the title wouldn't be shown when the user is not logged in (like in the login page)

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,13 +1,13 @@
 PontoPessoal.helpers do
 
-  def greeting name
+  def greeting name = nil
     greeting = case Time.now.hour
       when 00..11 then 'Bom dia'
       when 12..17 then 'Boa tarde'
       when 18..23 then 'Boa noite'
     end
 
-    "#{greeting}, #{name}!"
+    if name.nil? then greeting+'!' else "#{greeting}, #{name}!" end
   end
 
 end
